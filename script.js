1f64ba91a8c14d4231b62000baf315db01bf77dd
Kenfresno <Kfresno@proton.me>
2023-01-26 21:46:43 +0200

    Add function to select element from array for computer. Compare selection to argument and print result of game

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 const RPS = ["Rock", "Paper", "Scissors"];
+
+
 function getComputerChoice(){
     return RPS[Math.floor(Math.random()*RPS.length)];
+}
+function playRound(playerChoice){
+    let computerChoice = getComputerChoice();
+    if (playerChoice === computerChoice){
+        //console.log("player choice: " + playerChoice);
+        //console.log("computer choice: " + computerChoice);
+        return "Draw";
+    }
+    else if (computerChoice === "Paper") {
+        //console.log("player choice: " + playerChoice);
+        //console.log("computer choice: " + computerChoice);
+        return "Computer wins the round!";
+    }
+    else {
+        //console.log("player choice: " + playerChoice);
+        //console.log("computer choice: " + computerChoice);
+        return "Player wins the round!";
+    }
 }
