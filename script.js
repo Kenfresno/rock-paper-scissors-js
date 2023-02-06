677b5dd9fcd94de6b93c6728b27584582c20ec3e
Kenfresno <Kfresno@proton.me>
2023-02-06 18:13:32 +0200

    Add functionality to play a round from button click + add score counter

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 const RPS = ["Rock", "Paper", "Scissors"];
 let playerChoice ;
 let winCount = 0
+const para = document.createElement("p");
+const paraTwo = document.createElement("p");
+btnContainer = document.querySelector("body");
 function getComputerChoice(){
     return RPS[Math.floor(Math.random()*RPS.length)];
 }
 
-function myFunction(e){
-    playerChoice = "";
-    console.log(e.target.className);
+
+
+function game(e){
     playerChoice = e.target.className;
-    console.log("player chose: " + playerChoice);
-}
+    computerChoice = getComputerChoice();
 
-
-function game(){
-    
-    if (winCount > 0) {
-        console.log("Player wins with score of: " + winCount);
+    if (playerChoice == "Rock" && computerChoice == "Scissors" || playerChoice == "Scissors" && computerChoice == "Paper" || playerChoice == "Paper" && computerChoice == "Rock") {
+        paraTwo.textContent = "The player has chosen: " + playerChoice + " | The Computer has chosen: " + computerChoice + "!";
+        winCount ++; 
+        para.textContent = "Player wins with score of: " + winCount;
+        btnContainer.appendChild(paraTwo)
+        btnContainer.appendChild(para);
     }
-    else if (winCount < 0) {
-        console.log("Computer wins with score of: " + winCount);
+    else if (playerChoice == "Scissors" && computerChoice == "Rock" || playerChoice == "Paper" && computerChoice == "Scissors" || playerChoice == "Rock" && computerChoice == "Paper") {
+        paraTwo.textContent = "The player has chosen: " + playerChoice + " | The Computer has chosen: " + computerChoice + "!";
+        winCount --;
+        para.textContent = "Computer wins with score of: "+ winCount;
+        btnContainer.appendChild(paraTwo)
+        btnContainer.appendChild(para);
     }
     else {
-        console.log("Game Draw!");
+        paraTwo.textContent = "The player has chosen: " + playerChoice + " | The Computer has chosen: " + computerChoice + "!";
+        para.textContent = "Game Draw! Score sits at: " + winCount;
+        btnContainer.appendChild(paraTwo)
+        btnContainer.appendChild(para);
     }
 }
-window.addEventListener("click", myFunction)
+window.addEventListener("click", game);
